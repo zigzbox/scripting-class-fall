13df31f34ed73612a522b4d2435b996f8e4c770c
glen hecht <glen.hecht@gmail.com>
2015-09-24 09:11:22 -0600

    no message

---
+++ duckhunt/Assets/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+    int numEnemies = 5;
+	// Use this for initialization
+	void Start () {
+        for(int i = 0; i < numEnemies; i++)
+        {
+            Debug.Log("Creating enemy number:" + i);
+        }
+        	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ duckhunt/Assets/ifstate.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ifstate : MonoBehaviour {
+    float puppiesAwe = 50.0f;
+    float tomanylimitPuppies = 75.0f;
+    float notenoughlimitPuppies = 25.0f;
+    // Use this for initialization
+    void Start() {
+        if (Input.GetKeyDown(KeyCode.Space))
+            puppiesAwe();
+        puppiesAwe -= Time.deltaTime * 5f;
+
+    }
+    void yayPuppiesTest()
+    {
+        else if (puppiesAwe > tomanylimitPuppies)
+        {
+            print("too many puppies");
+        }
+        else if (puppiesAwe < notenoughlimitPuppies)
+        {
+            print("not enough puppies");
+        }
+        else
+        {
+            print("yay puppies");
+        }
+    }
+}
